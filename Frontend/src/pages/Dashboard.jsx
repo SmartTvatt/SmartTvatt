5cb11cca759b28e0de21f03ef69277e80c523eed
jafgo0 <2jafar.gohari@gmail.com>
2026-09-22 16:35:05 +0200

    created Fonted folder..

---
+++ Frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+
+import './Dashboard.css'
+
+
+export default function Dashboard() 
+{ 
+    return(
+
+        <div>
+            <header>    
+                <h1>SmartTvätt</h1>
+                <div>
+                    <span>Username</span>
+                    <span>   Gjutrigatan 26 </span>
+                    <button>Logga ut</button>   
+                </div>
+            </header>
+
+
+        </div>
+    )
+}
